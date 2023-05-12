7451e231abc4a663b10120f72d2149817f67585f
mirian228 <pariksini@mail.ru>
2023-05-12 13:39:23 +0400

    Simple HTTP Requests without implementation, can be seen using POSTMAN

--- src/main/java/com/restservices/demo/UsersController.java
+++ src/main/java/com/restservices/demo/UsersController.java
@@ -1,5 +1,10 @@
 package com.restservices.demo;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +12,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/users")
 public class UsersController {
 
+	@GetMapping()
+	public String getUsers() {
+		return "http GET request was sent";
+	}
+
+	@GetMapping(path = "/{userId}")
+	public String getUser(@PathVariable String userId) {
+		return "http GET request was sent for userid: " + userId;
+	}
+
+	@PostMapping
+	public String createUser() {
+		return "http POST request was sent";
+
+	}
+
+	@DeleteMapping
+	public String deleteUser() {
+		return "http DELETE request was sent";
+
+	}
+
+	@PutMapping
+	public String updateUser() {
+		return "http PUT request was sent";
+
+	}
 }
