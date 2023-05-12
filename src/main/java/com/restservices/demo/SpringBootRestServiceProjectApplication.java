6e4b0e2215fb06dbe5ccb16b4c5fb18a067eb14b
mirian228 <pariksini@mail.ru>
2023-05-12 13:21:04 +0400

    Starter Project specs

---
+++ src/main/java/com/restservices/demo/SpringBootRestServiceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.restservices.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestServiceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestServiceProjectApplication.class, args);
+	}
+
+}
