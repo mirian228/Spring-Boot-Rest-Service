5b6988df647d862169a7f55b8a68953f17c817e9
mirian228 <pariksini@mail.ru>
2023-05-12 16:08:43 +0400

    Implemented Service Class with HashMap

--- src/main/java/com/restservices/demo/services/CountryService.java
+++ src/main/java/com/restservices/demo/services/CountryService.java
@@ -1,7 +1,10 @@
 package com.restservices.demo.services;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
+import com.restservices.demo.controllers.AddResponse;
 import com.restservices.demo.model.Country;
 
 public class CountryService {
@@ -21,4 +24,56 @@ public class CountryService {
 
 	}
 
+	public List getAllCountries() {
+		List countries = new ArrayList(countryIdMap.values());
+		return countries;
+	}
+
+	public Country getCountryById(int id) {
+		return countryIdMap.get(id);
+	}
+
+	public Country getCountryByName(String name) {
+		String countryName = null;
+		int i;
+		Country result = null;
+		for (i = 0; i < countryIdMap.size(); i++) {
+			countryName = countryIdMap.get(i).getCountryName();
+			if (countryName == name) {
+				result = countryIdMap.get(i);
+			}
+		}
+
+		return result;
+	}
+
+	public Country addCountry(Country country) {
+		return countryIdMap.put(getMaxId(), country);
+	}
+
+	// Utility Method to get max id
+	public static int getMaxId() {
+		int max = 0;
+		for (int id : countryIdMap.keySet()) {
+			if (max <= id) {
+				max = id;
+			}
+		}
+		return max + 1;
+	}
+
+	public Country updateCountry(Country country) {
+		if (country.getId() > 0) {
+			countryIdMap.put(country.getId(), country);
+		}
+		return country;
+	}
+
+	public AddResponse deleteCountry(int id) {
+		countryIdMap.remove(id);
+		AddResponse response = new AddResponse();
+		response.setMessage("Country Deleted");
+		response.setId(id);
+		return response;
+	}
 }
