396dfaeab0f437486a52897a7ab107915b3ff801
mirian228 <pariksini@mail.ru>
2023-05-12 15:34:19 +0400

    Using hashmap as a temporary database replacement

--- src/main/java/com/restservices/demo/model/Country.java
+++ src/main/java/com/restservices/demo/model/Country.java
@@ -3,8 +3,15 @@ package com.restservices.demo.model;
 public class Country {
 
 	private int id;
-	private String countryName;  
+	private String countryName;
 	private String countryCapital;
+
+	public Country(int id, String countryName, String countryCapital) {
+		super();
+		this.id = id;
+		this.countryName = countryName;
+		this.countryCapital = countryCapital;
+	}
 
 	public int getId() {
 		return id;
--- src/main/java/com/restservices/demo/services/CountryService.java
+++ src/main/java/com/restservices/demo/services/CountryService.java
@@ -7,12 +7,18 @@ import com.restservices.demo.model.Country;
 public class CountryService {
 
 	static HashMap<Integer, Country> countryIdMap;
-	
-	
-	
-	
-	
-	
-	
-	
+
+	public CountryService() {
+		countryIdMap = new HashMap<Integer, Country>();
+
+		Country indiaCountry = new Country(1, "India", "Delhi");
+		Country usaCountry = new Country(1, "USA", "Washington");
+		Country ukCountry = new Country(1, "UK", "London");
+
+		countryIdMap.put(1, indiaCountry);
+		countryIdMap.put(2, usaCountry);
+		countryIdMap.put(3, ukCountry);
+
+	}
+
 }
